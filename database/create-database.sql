9be83c7be9c4b3d33bdbe387d27fbbc80d2364b0
Bar Einav <bareinav10@gmail.com>
2023-10-30 00:25:47 +0200

    update database to work better and using chapter title + image

--- database/create-database.sql
+++ database/create-database.sql
@@ -7,22 +7,32 @@ CREATE TABLE `courses` (
 	`course_title` VARCHAR(32) NOT NULL,
     `course_description` VARCHAR(256) DEFAULT NULL,
     `course_image` VARCHAR(256) NOT NULL,
-    `current_div` SMALLINT UNSIGNED DEFAULT 1 NOT NULL,
-    `current_progress` MEDIUMINT UNSIGNED DEFAULT 1 NOT NULL,
-    `course_html` MEDIUMTEXT NOT NULL,
+    `current_chapter` MEDIUMINT UNSIGNED DEFAULT 1 NOT NULL,
     PRIMARY KEY (`course_id`)
+);
+
+CREATE TABLE `course_chapters` (
+	`course_id` INT NOT NULL,
+    `chapter_number` MEDIUMINT NOT NULL,
+    `chapter_title` VARCHAR(32) NOT NULL,
+    `chapter_image` VARCHAR(256) NOT NULL,
+    `chapter_progress` MEDIUMINT UNSIGNED DEFAULT 1 NOT NULL,
+    `chapter_html` MEDIUMTEXT NOT NULL,
+    PRIMARY KEY (`course_id`, `chapter_number`)
 );
 
 CREATE TABLE `course_answers` (
 	`course_id` INT NOT NULL,
+    `chapter_number` INT NOT NULL,
 	`question_id` VARCHAR(16) NOT NULL,
 	`answer_hash` VARCHAR(60) NOT NULL,
-    PRIMARY KEY (`course_id`, `question_id`)
+    PRIMARY KEY (`course_id`, `chapter_number`, `question_id`)
 );
 
 CREATE TABLE `course_options` (
 	`course_id` INT NOT NULL,
+    `chapter_number` INT NOT NULL,
 	`question_id` VARCHAR(16) NOT NULL,
 	`question_option` VARCHAR(60) NOT NULL,
-    PRIMARY KEY (`course_id`, `question_id`)
+    PRIMARY KEY (`course_id`, `chapter_number`, `question_id`)
 );
